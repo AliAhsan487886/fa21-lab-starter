a8377d32bc99815445983603f52a0ec83816d774
Ali Ahsan <aliahsan487886@gmal.com>
2025-07-09 11:44:47 +0500

    Exercise_04

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,18 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *slow_ptr = head;
+    node *fast_ptr = head;
+
+    // Loop until fast_ptr reaches the end of the list
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+
+        if (slow_ptr == fast_ptr) {
+            return 1; // Cycle detected
+        }
+    }
+
+    return 0; // No cycle detected
 }
